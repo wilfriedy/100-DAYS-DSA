52a0862d6017995d601686524ec1da81de9c818b
wilfriedy <wilfriedakpe3@gmail.com>
2024-05-23 22:33:02 +0100

    linked list operations

--- beginner lvl/linkedlist/basics-linked-list.py
+++ beginner lvl/linkedlist/basics-linked-list.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.data = value
         self.next = None
 
 class LinkedList:
@@ -23,10 +23,42 @@ class LinkedList:
             currentNode = currentNode.next
         # print(currentNode.data)
 
+    def prependNode(self,value):
+        new_node = Node(value)
+        temp = self.head
+        new_node.next = temp
+        self.head = new_node
+
+    def getPositionByValue(self, value):
+        position = 1
+        currentNode = self.head
+        while currentNode is not None:
+            if currentNode.data == value:
+                return position
+            currentNode  = currentNode.next
+            position +=1
+        return None
+    def getValueByPosition(self, position):
+        if position <= 0:
+            return "Out of range"
+        counter = 1
+        currentNode = self.head
+
+        while currentNode is not None:
+            if counter == position:
+                return currentNode.data
+            currentNode = currentNode.next
+            counter += 1
+
+        return 'Out of range'
+
 
 linkedListSolution = LinkedList()
 linkedListSolution.addItem(2)
 linkedListSolution.addItem(3)
 linkedListSolution.addItem(5)
+linkedListSolution.prependNode(10)
 print(linkedListSolution.printLinkedList())
+print(linkedListSolution.getPositionByValue(20))
+print(linkedListSolution.getValueByPosition(5))
 # print(linkedListSolution.head.data, end=f" -> next : {linkedListSolution.head.next.data}")
