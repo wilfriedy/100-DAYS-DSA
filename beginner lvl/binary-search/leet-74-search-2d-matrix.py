09f737cb40e95fce0ab2b2017c345b0a82f77b15
wilfriedy <wilfriedakpe3@gmail.com>
2024-06-03 19:04:43 +0100

    leet-74:binsearch

---
+++ beginner lvl/binary-search/leet-74-search-2d-matrix.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(len(matrix)):
+            currentRow = matrix[i]
+            left, right = 0, len(currentRow) -1
+            if currentRow[left] <= target <= currentRow[right]:
+                while left <= right:
+                    mid = (left + right) // 2
+                    if currentRow[mid] > target:
+                        right = mid -1
+                    elif currentRow[mid] < target:
+                        left = mid + 1
+                    else:
+                        return True
+
+            else:
+                continue
+
+        return False
