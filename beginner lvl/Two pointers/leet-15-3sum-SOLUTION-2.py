df5a6a97c14f3b869605ebd999488caa48a4901f
wilfriedy <wilfriedakpe3@gmail.com>
2024-06-28 19:39:56 +0100

    two-pointers:leet-15

---
+++ beginner lvl/Two pointers/leet-15-3sum-SOLUTION-2.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i -1]:
+                continue
+
+            j = i +1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+
+                    j += 1
+                    k -= 1
+        return result
+
+solution = Solution()
+print(solution.threeSum([-1,0,1,2,-1,-4]))
