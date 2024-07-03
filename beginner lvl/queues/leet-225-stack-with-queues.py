4363997d2a8d376cdfc55884f4fc0e54f111eb5d
wilfriedy <wilfriedakpe3@gmail.com>
2024-07-03 20:06:28 +0100

    queues:leet-225

--- beginner lvl/queues/leet-225-stack-with-queues.py
+++ beginner lvl/queues/leet-225-stack-with-queues.py
@@ -1,13 +1,19 @@
 from collections import deque
+
+
 class MyStack:
 
     def __init__(self):
         self.queue = deque()
+
     def push(self, x: int) -> None:
         self.queue.extendleft([x])
+
     def pop(self) -> int:
         return self.queue.popleft()
+
     def top(self) -> int:
+        return self.queue[0]
 
     def empty(self) -> bool:
-        pass
+        return True if len(self.queue) == 0 else False
