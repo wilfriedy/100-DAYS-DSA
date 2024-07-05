61edeae31a832e49621768de21ba85265145d82f
wilfriedy <wilfriedakpe3@gmail.com>
2024-07-05 19:23:54 +0100

    revison:sliding-window::leet-3 and strings::leet-242:valid-anagram

--- beginner lvl/Arrays - Strings/leet-242-valid-anangram.py
+++ beginner lvl/Arrays - Strings/leet-242-valid-anangram.py
@@ -5,19 +5,19 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         countS = Counter(s)
         countT = Counter(t)
-
-        for char, count in countT.items():
-            if char not in countS or not count == countS[char]:
-                return False
-
-        for char, count in countS.items():
-            if char not in countT or not count == countT[char]:
-                return False
-
-
-        return True
+        return countS == countT
+        # for char, count in countT.items():
+        #     if char not in countS or not count == countS[char]:
+        #         return False
+        #
+        # for char, count in countS.items():
+        #     if char not in countT or not count == countT[char]:
+        #         return False
+        #
+        #
+        # return True
 
 solution = Solution()
-s = "anagramy"
+s = "anagram"
 t = "nagaram"
 print(solution.isAnagram(s,t))
--- playground.py
+++ playground.py
@@ -1,4 +1,16 @@
-obj1 = {}
-obj2 = {}
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charDict = {}
+        maxSubstring= 0
+        left = 0
+        for right in range(len(s)):
+            if s[right] in charDict and left <= charDict[s[right]]:
+                left = charDict[s[right]] + 1
+            charDict[s[right]] = right
+            lenOfSubStr = right - left + 1
+            maxSubstring = max(maxSubstring, lenOfSubStr)
 
-print(obj1 == obj2)
+        return maxSubstring
+
+solution = Solution()
+print(solution.lengthOfLongestSubstring("abcabcbb"))
