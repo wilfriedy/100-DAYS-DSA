23233fd804c3ca8b70862561247d82a9adfa48b2
wilfriedy <wilfriedakpe3@gmail.com>
2024-07-20 18:48:21 +0100

    bst::leet-100

---
+++ beginner lvl/binary-trees/leet-100-same-tree.py
@@ -0,0 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+        elif p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
