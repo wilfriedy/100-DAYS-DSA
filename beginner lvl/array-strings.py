b0502b0577188fbb35c2e8698218182a8413e945
wilfriedy <wilfriedakpe3@gmail.com>
2024-07-21 20:20:26 +0100

    BST::leet-100

--- beginner lvl/array-strings.py
+++ beginner lvl/array-strings.py
@@ -1,5 +1,19 @@
-nums = [1,2,3,4,5]
-name = "wilfred"
-nums.append(6)
+from typing import List
+def oddString(words: List[str]) -> str:
+    n = len(words)
+    differences = []
+    for i in range(n):
+        temp = []
+        for j in range(n-1):
+            dif = ord(words[i][j+1]) - ord(words[i][j])
+            temp.append(dif)
 
-print(nums)
+        differences.append(temp)
+
+    print(differences.count(differences[2]))
+
+    return differences
+
+words = ["adc","wzy","abc"]
+print(oddString(words))
+
