83e6aeffb4913f6c87c709c18411d63cb3d820e8
wilfriedy <wilfriedakpe3@gmail.com>
2024-07-23 22:54:26 +0100

    sorting::basics

---
+++ beginner lvl/sorting-algo/basics.py
@@ -0,0 +1,18 @@
+# selection sort implementation
+from typing import List
+
+
+def selection_sort(nums: List[int]) -> List[int]:
+    n = len(nums)
+
+    for i in range(n - 1):
+        current_min = i
+        for j in range(i + 1, n):
+            if nums[current_min] > nums[j]:
+                current_min = j
+
+            nums[i], nums[current_min] = nums[current_min], nums[i]
+
+    return nums
+
+print(selection_sort([66,8,100,1,3,0]))
