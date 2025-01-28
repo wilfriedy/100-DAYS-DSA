884f446feaaed7744588ca18160725695a370658
wilfriedy <wilfriedakpe3@gmail.com>
2025-01-28 21:34:34 +0100

    leetcode 283

--- 2025/beginner/arrays/leet-283-move-zeroes.py
+++ 2025/beginner/arrays/leet-283-move-zeroes.py
@@ -3,5 +3,12 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        if len(nums) > 1:
-            for
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        numsOfZeros = 0
+        while 0 in nums:
+            nums.remove(0)
+            numsOfZeros +=1
+
+        nums.extend([0] * numsOfZeros)
